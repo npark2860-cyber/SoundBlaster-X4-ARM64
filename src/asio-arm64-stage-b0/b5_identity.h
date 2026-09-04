211f7860bf7210db95a36f9422b9448dd9e2a8b3
npark2860-cyber <npark2860@gmail.com>
2026-09-04 10:46:04 +0900

    Add B5 side-by-side ASIO identity

---
+++ src/asio-arm64-stage-b0/b5_identity.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <windows.h>
+
+inline constexpr GUID CLSID_X4_ARM64_ASIO_B5 =
+    {0x4bdf4da4, 0x27f8, 0x47c3, {0xbb, 0xec, 0x6a, 0x74, 0x5c, 0x11, 0x5b, 0x49}};
+
+inline constexpr wchar_t kX4AsioB5ClsidString[] = L"{4BDF4DA4-27F8-47C3-BBEC-6A745C115B49}";
+inline constexpr wchar_t kX4AsioB5RegistryName[] = L"Sound Blaster X4 ARM64 ASIO B5";
+inline constexpr wchar_t kX4AsioB5Description[] = L"Sound Blaster X4 native ARM64 ASIO B5";
+inline constexpr char kX4AsioB5DriverName[] = "Sound Blaster X4 ARM64 B5";
